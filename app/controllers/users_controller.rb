bfec3100747d4946ec21cc6370e51b77970a4413
Navi Mann <navdeep.k.mann@gmail.com>
2017-10-10 23:29:35 -0400

    created logic for eval calls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
-
-
 class UsersController < ApplicationController
-  require 'sicuro'
+  # require 'sicuro'
+  require 'open3'
+
   def index
 
   end
@@ -9,8 +9,14 @@ class UsersController < ApplicationController
   def create
     @code= params[:userCode][:code]
 
-    render json: Sicuro.eval(@code).stdout
-
+    # render json:
+      Open3.popen3("ruby -e '#{@code}'") do |stdin, stdout, stderr|
+        render json:{ stdout: stdout.read, stderr: stderr.read}
+        # puts "stderr is:" + stderr.read
+      end
+      # stdin, stdout, stderr = Open3.popen3("ruby","-e", "'#{@code}'")
+      #   puts stdout.read
+    # Sicuro.eval(@code).stdout
 
   end
 
