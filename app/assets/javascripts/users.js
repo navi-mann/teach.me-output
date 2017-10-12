07cf4ef24af5dbac30d2a197639ef9b3cff48d91
tenzin2017 <tenzinleo@gmail.com>
2017-10-12 19:40:52 -0400

    included remote: true on form_tag, which does ajax call

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
    var code = $('.codemirror-textarea')[0];
    var editor = CodeMirror.fromTextArea(code, {
-   	lineNumbers :true,
-   	mode: "text/x-ruby"
+     lineNumbers :true,
+     mode: "text/x-ruby"
    })
+
+
+
 });
