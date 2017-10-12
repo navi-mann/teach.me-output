c0760788771365f76021f7699970101fbb49c078
Navi Mann <navdeep.k.mann@gmail.com>
2017-10-12 16:53:12 -0400

    changed logic using Sicuro gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  # require 'sicuro'
-  require 'open3'
+  require 'sicuro'
+  # require 'open3'
 
   def index
 
@@ -9,14 +9,16 @@ class UsersController < ApplicationController
   def create
     @code= params[:userCode][:code]
 
-    # render json:
-      Open3.popen3("ruby -e '#{@code}'") do |stdin, stdout, stderr|
-        render json:{ stdout: stdout.read, stderr: stderr.read}
-        # puts "stderr is:" + stderr.read
-      end
-      # stdin, stdout, stderr = Open3.popen3("ruby","-e", "'#{@code}'")
-      #   puts stdout.read
-    # Sicuro.eval(@code).stdout
+      # Open3.popen3("ruby -e ' #{@code}'") do |stdin, stdout, stderr|
+      #   render json:{ stdout: stdout.read, stderr: stderr.read}
+      # end
+      codeResult = Sicuro.eval(@code)
+      render json:{
+        result: codeResult.stdout,
+        error: codeResult.stderr
+
+      }
+
 
   end
 
