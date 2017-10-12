e268b899c69c2fc5fde896df3f539bb47a547b50
tenzin2017 <tenzinleo@gmail.com>
2017-10-12 12:56:47 -0400

    cleaned a code little bit in create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # require 'sicuro'
+
   require 'open3'
 
   def index
@@ -7,16 +7,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @code= params[:userCode][:code]
+    @code = params[:userCode][:code]
 
-    # render json:
-      Open3.popen3("ruby -e '#{@code}'") do |stdin, stdout, stderr|
+    Open3.popen3("ruby -e '#{@code}'") do |stdin, stdout, stderr|
         render json:{ stdout: stdout.read, stderr: stderr.read}
-        # puts "stderr is:" + stderr.read
+
       end
-      # stdin, stdout, stderr = Open3.popen3("ruby","-e", "'#{@code}'")
-      #   puts stdout.read
-    # Sicuro.eval(@code).stdout
 
   end
 
